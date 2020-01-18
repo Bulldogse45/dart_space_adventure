8af30e5a0839ed0c5d0543af4cba8bb4e8bb75f5
Matthew Fair <matt.j.fair@gmail.com>
2020-01-18 15:55:03 -0500

    initial

---
+++ bin/main.dart
@@ -0,0 +1,15 @@
+import 'package:dart_space_adventure/dart_space_adventure.dart' as dart_space_adventure;
+
+void main(List<String> arguments) {
+  print('Welcome to the Solar System!');
+  print('There are 8 planets to explore!');
+  print('What is your name?');
+  print('Matt');
+  print('Nice to meet you, Matt.  Let\'s go on an adventure!');
+  print('Shall I randomly choose a planet for you?');
+  print('N');
+  print('Name the planet you would like to visit.');
+  print('Neptune');
+  print('Traveling to Neptune');
+  print('Arriving at Neptune.  A very cold planet furthest from the sun');
+}
