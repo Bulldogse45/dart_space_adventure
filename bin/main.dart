e76c4381278b723fef1bc58c41042ab1c77b2379
Matthew Fair <matt.j.fair@gmail.com>
2020-01-19 22:41:52 -0500

    working

--- bin/main.dart
+++ bin/main.dart
@@ -1,26 +1,36 @@
+import 'dart:convert';
+import 'dart:io';
 import 'package:dart_space_adventure/dart_space_adventure.dart';
 
-const system = "Solar System";
-const planetData = {
-  "Mercury": "This is a hot planet, closest to the sun",
-  "Venus": "This is a hot planet with runaway greenhouse gases, second closest to the sun",
-  "Earth": "This is a hot planet with runaway greenhouse gases, third closest to the sun",  
-  "Mars": "This is a hot planet with runaway greenhouse gases, fourth closest to the sun", 
-  "Jupiter": "This is a hot planet with runaway greenhouse gases, fifth closest to the sun",  
-  "Saturn": "This is a hot planet with runaway greenhouse gases, sixth closest to the sun",  
-  "Uranus": "This is a hot planet with runaway greenhouse gases, seventh closest to the sun",  
-  "Neptune": "This is a hot planet with runaway greenhouse gases, eigth closest to the sun"
-};
+// const system = "Solar System";
+// const planetData = {
+//   "Mercury": "This is a hot planet, closest to the sun",
+//   "Venus": "This is a hot planet with runaway greenhouse gases, second closest to the sun",
+//   "Earth": "This is a hot planet with runaway greenhouse gases, third closest to the sun",  
+//   "Mars": "This is a hot planet with runaway greenhouse gases, fourth closest to the sun", 
+//   "Jupiter": "This is a hot planet with runaway greenhouse gases, fifth closest to the sun",  
+//   "Saturn": "This is a hot planet with runaway greenhouse gases, sixth closest to the sun",  
+//   "Uranus": "This is a hot planet with runaway greenhouse gases, seventh closest to the sun",  
+//   "Neptune": "This is a hot planet with runaway greenhouse gases, eigth closest to the sun"
+// };
 
 
 void main(List<String> arguments) {
-  SpaceAdventure(
-    planetarySystem: PlanetarySystem(
-      name: system, 
-      planets: mockPlanets()
-    )).start();
+  var fileData = File(arguments[0]);
+  PlanetarySystem planetarySystem;
+  fileData.readAsString().then((String systemDataAsString) {
+    SpaceAdventure(planetarySystem: createSystem(systemDataAsString)).start();
+  }); 
 }
 
-List<Planet> mockPlanets() {
-   return planetData.entries.map( (e) => Planet(name: e.key, description: e.value )).toList();
+PlanetarySystem createSystem(String systemStringData) {
+  final systemData = jsonDecode(systemStringData);
+  List<Planet> planets = [];
+  systemData['planets'].forEach( (thing){
+    planets.add(Planet(name: thing['name'], description: thing['description']));
+  });
+  return PlanetarySystem(
+    name: systemData['name'],
+    planets: planets
+  );
 }
